9675308889de08c4ebc0da28efb885b0a1d46e00
MOHAMMED ADEL <sourceyea@gmail.com>
2019-10-05 04:34:26 +0800

    updated

--- OXV.pl
+++ OXV.pl
@@ -2,26 +2,30 @@
 
 use HTTP::Request; # If not installed, do -> su => cpan => install HTTP::Request
 use LWP::UserAgent; # If not installed, do -> su => cpan => install LWP::UserAgent
-use Term::ANSIColor; # if not installed, do -> su => cpan => install Term::ANSIColor
+use Term::ANSIColor; # If not installed, do -> su => cpan => install Term::ANSIColor
+use Win32::Console::ANSI; # If not installed, do -> su => cpan => install Win32::Console::ANSI
 
 print "\n";
-print color("red"), "\n                       @ [By] MOHAMMED ADEL \n\n", color("reset");
-print color("blue"),"                     [+] Examples of outputs [+]\n", color("reset");
-print color("blue"), "           [T] with URL -> path is working perfectly. \n";
-print "           [F] with URL -> path is redirected to login page.\n";
-print "           [T] with NO URL -> path doesn't exists.\n\n", color("reset");
-print color("white"), "           NOTICE : If the output is [T] with URL,\n", color("reset");
-print color("white"), "           then the URL leads to access\n", color("reset");
-print color("white"), "           information without logging in. ;)\n\n", color("reset");
-print color("red"), "      ------------------------------------------------------------\n\n", color("reset");
-print color("blue"), "                     [+] Example of input [+]\n";
-print "           [*] https://example.oracle.com/ords/f?p=\n\n\n", color("reset");
-print color("yellow"), "Enter URL --> ", color("reset");
+print color("red"), <<MD;
+                             _..:::.._
+                           .:::::/|::::.
+                          ::::::/ V|:::::
+                         ::::::/'  |::::::
+                         ::::<_,   (::::::
+                          :::::|    \::::
+                           '::/      \:' ~ By MOHAMMED ADEL 
+	~ Oracle Application Express Directory Brute Force ~
+					
+					
+MD
+print color("blue"), "      ------------------------------------------------------------\n\n", color("reset");
+print "           [*] Example: https://example.oracle.com/ords/f?p=\n\n\n", color("reset");
+print color("yellow"), "[-] LINK || URL --> ", color("reset");
 my $url1 = <STDIN>;
 chomp $url1;
 my $i;
-my $start_num = 340; # minimum number is 1 .
-my $end_num = 345; # As many number as you want ;)
+my $start_num = 100; # minimum number is 1 .
+my $end_num = 9999; # the higher the better
 
 for($i = $start_num; $i <= $end_num; $i++){
 
@@ -30,13 +34,19 @@ my $ua = LWP::UserAgent->new;
 my $request  = HTTP::Request->new( GET => $url );
 my $response = $ua->request($request);
 my $final = $response->header('Location');
+my $final_code = $response->status_line;
 
 if ($final =~ m/:LOGIN_DESKTOP:/)
  {
- print color("red"), "\n\n[F] $final\n", color("reset");
+ print color("red"), "\n\n[FAILED] $final\n", color("reset");
 }
-else
-{
-  print color("green"), "\n\n[T] $final\n", color("reset");
-}
+    else
+    {
+        next if($final_code =~ m/200 OK/);
+
+	       if($final_code =~ m/302 Found/)
+		      {
+  		        print color("green"), "\n\n[SUCCESS] $final\n", color("reset");
+                }
+        }
 }
